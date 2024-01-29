6c9c835fff883b1adae3dfe0f9f7a1c2026baf12
ToyinM <toyinmuhammed50@gmail.com>
2024-01-29 14:42:04 +0100

    code cleanup + advanced twosums

--- two-sums/Program.cs
+++ two-sums/Program.cs
@@ -5,8 +5,8 @@ int target;
 
 //First example
 arr = [2, 7, 11, 15]; target = 9;
-Console.WriteLine("First example: " + Solution.TwoSum(arr, target));
+Console.WriteLine("First example: " + Solution.TwoSumAdvanced(arr, target));
 
 //Second example
 arr = [3, 2, 4]; target = 6;
-Console.WriteLine("Second example: " + Solution.TwoSum(arr, target));
+Console.WriteLine("Second example: " + Solution.TwoSumAdvanced(arr, target));
--- two-sums/src/Solution.cs
+++ two-sums/src/Solution.cs
@@ -6,15 +6,29 @@ public static class Solution
         //This implements two sum in O(n^2) time
         int[] result = new int[2];
         for (int i = 0; i < nums.Length; i++)
-        {
             for (int j = i + 1; j < nums.Length; j++)
-            {
                 if ((nums[i] + nums[j]) == target)
                 {
                     result = [i, j];
+                    break;
                 }
-            }
+        return Prettify(result);
+    }
+
+    public static string TwoSumAdvanced(int[] nums, int target)
+    {
+        //define a hashmap of integers
+        Dictionary<int, int> hashMap = [];
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            if (hashMap.TryGetValue(diff, out int value))
+                result = [value, i];
+            else
+                hashMap.TryAdd(nums[i], i);
         }
+
         return Prettify(result);
     }
 
@@ -27,9 +41,7 @@ public static class Solution
 
             // Add a comma and space if it's not the last element
             if (i < nums.Length - 1)
-            {
                 result += ",";
-            }
         }
         result += "]";
         return result;
